c0d87771bfeca0ccc16845202c48b290196a30f4
user <eunssol01@gmail.com>
2021-07-05 15:46:55 +0900

    ㅇㄹㅇㄹ

--- javaTest/src/javaTest/Test.java
+++ javaTest/src/javaTest/Test.java
@@ -9,7 +9,7 @@ public class Test {
 		System.out.println("Test4");
 		System.out.println("Test6");
 		System.out.println("Test5");
-		System.out.println("Test9");
+		System.out.println("Test10");
 	}
 
 }
